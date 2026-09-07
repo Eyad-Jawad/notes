54ae317b8dbaa7e4f080f48308ae91e0f5fb79fb
Eyad Jawad <ayadmansr@gmail.com>
2026-09-07 21:35:03 +0300

    Add html to reference to fix some broken links

--- src/programming/main.py
+++ src/programming/main.py
@@ -97,7 +97,7 @@ def main() -> None:
 def find_file_relative_path(filename: str, files: list[Path], root: Path) -> str:
     for file in files:
         if filename in str(file):
-            return quote(str(file.relative_to(root))[:-3])
+            return quote(str(file.relative_to(root))[:-3]) + ".html"
     return ""
 
 if __name__ == "__main__":
